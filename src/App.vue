486a022f5cdf0c7631cffa01ae9ef9380575b6ba
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-06-03 13:59:30 +0200

    scoping styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>{{ title }}</h1>
+  <Modal />
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">click me</button> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { Modal },
+  data() {
+    return {
+      title: "My first Vue App :)",
+    };
+  },
+  methods: {
+    // handleClick() {
+    //   console.log(this.$refs.name)
+    //   this.$refs.name.classList.add('active')
+    //   this.$refs.name.focus()
+    // }
   },
 };
 </script>
@@ -22,5 +34,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 10px;
+  display: inline-block;
 }
 </style>
